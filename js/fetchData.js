c9492c9742656d0a56a7fdc67debf18f668bfbf4
Youyou <puyouyou@gmail.com>
2024-05-22 16:39:30 +0200

    try sth....

--- js/fetchData.js
+++ js/fetchData.js
@@ -10,7 +10,7 @@ const fetchData = async ({ queryKey }) => {
   const host = "127.17.0.1";
   const port = "8080";
   // const baseurl = `${proto}://${host}:${port}/wp-json/wp/v2`;
-  const baseurl = "https://bv2024wordpress.42web.io/wp-json/wp/v2";
+  const baseurl = "http://cors.io/?https://bv2024wordpress.42web.io/wp-json/wp/v2";
   // const baseurl1="http://127.17.0.1:8080/wp-json/wp/v2"
   if (taxonomy == "" && data == "projekte") {
     apiRes = await fetch(
