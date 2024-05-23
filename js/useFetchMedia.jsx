ac9470115f48b7dfb1f7f0a9e87ef3467c20f2cd
Youyou <puyouyou@gmail.com>
2024-05-23 20:12:06 +0200

    test hook 2

--- js/useFetchMedia.jsx
+++ js/useFetchMedia.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import Spinner from "./Spinner";
 import fetchData from "./fetchData";
-export function useFetchMedia( imgid, default_media )  {
+export function useFetchMedia(imgid, default_media) {
   const navigate = useNavigate();
   const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
   if (pic.isLoading) {
@@ -12,7 +12,7 @@ export function useFetchMedia( imgid, default_media )  {
     navigate("/NotMatch404");
   }
   // let servicePic = "imgs/category/forschung.png";
-  let media =default_media;
+  let media = default_media;
   let image = pic.data.source_url;
   console.log(pic);
   if (image) {
