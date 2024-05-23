f2589d15300d47575fdef10362f5ba125ffe7b4f
Youyou <puyouyou@gmail.com>
2024-05-23 20:21:57 +0200

    test hook 5

--- js/ServicePicComp.jsx
+++ js/ServicePicComp.jsx
@@ -1,32 +1,30 @@
 import { useFetchMedia } from "./useFetchMedia";
-import { useQuery } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import Spinner from "./Spinner";
-import fetchData from "./fetchData";
+// import { useQuery } from "@tanstack/react-query";
+// import { useNavigate } from "react-router-dom";
+// import Spinner from "./Spinner";
+// import fetchData from "./fetchData";
 const ServicePicComp = ({ imgid, picName }) => {
-  console.log(imgid);
-  console.log({ imgid });
   const media = useFetchMedia(imgid, "imgs/category/forschung.png");
   console.log(media);
-  const navigate = useNavigate();
-  const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
-  if (pic.isLoading) {
-    return <Spinner></Spinner>;
-  }
-  if (pic.isError) {
-    navigate("/NotMatch404");
-  }
-  let servicePic = "imgs/category/forschung.png";
-  let image = pic.data.source_url;
-  console.log(pic);
-  if (image) {
-    servicePic = image;
-  }
+  // const navigate = useNavigate();
+  // const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
+  // if (pic.isLoading) {
+  //   return <Spinner></Spinner>;
+  // }
+  // if (pic.isError) {
+  //   navigate("/NotMatch404");
+  // }
+  // let servicePic = "imgs/category/forschung.png";
+  // let image = pic.data.source_url;
+  // console.log(pic);
+  // if (image) {
+  //   servicePic = image;
+  // }
 
   return (
     <div className="picContainer">
       <div className="service_pic">
-        <img src={servicePic} alt={picName} />
+        <img src={media} alt={picName} />
         <div className="service_pic_caption">
           <div className="service_pic_text">{picName}</div>
         </div>
--- js/useFetchMedia.jsx
+++ js/useFetchMedia.jsx
@@ -3,9 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Spinner from "./Spinner";
 import fetchData from "./fetchData";
 export function useFetchMedia(imgid, default_media) {
-  console.log(imgid);
   const navigate = useNavigate();
-  const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
+  const pic = useQuery(["pic", `media`, imgid], fetchData);
   if (pic.isLoading) {
     return <Spinner></Spinner>;
   }
