be34785761abbc9c4b714cd9a99f503ca8dd2d6c
Youyou <puyouyou@gmail.com>
2024-05-23 20:17:31 +0200

    test hook 4

--- js/ServicePicComp.jsx
+++ js/ServicePicComp.jsx
@@ -1,25 +1,32 @@
 import { useFetchMedia } from "./useFetchMedia";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import fetchData from "./fetchData";
 const ServicePicComp = ({ imgid, picName }) => {
+  console.log(imgid);
+  console.log({ imgid });
   const media = useFetchMedia(imgid, "imgs/category/forschung.png");
-  // const navigate = useNavigate();
-  // const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
-  // if (pic.isLoading) {
-  //   return <Spinner></Spinner>;
-  // }
-  // if (pic.isError) {
-  //   navigate("/NotMatch404");
-  // }
-  // let servicePic = "imgs/category/forschung.png";
-  // let image = pic.data.source_url;
-  // console.log(pic);
-  // if (image) {
-  //   servicePic = image;
-  // }
+  console.log(media);
+  const navigate = useNavigate();
+  const pic = useQuery(["pic", `media`, `${imgid}`], fetchData);
+  if (pic.isLoading) {
+    return <Spinner></Spinner>;
+  }
+  if (pic.isError) {
+    navigate("/NotMatch404");
+  }
+  let servicePic = "imgs/category/forschung.png";
+  let image = pic.data.source_url;
+  console.log(pic);
+  if (image) {
+    servicePic = image;
+  }
 
   return (
     <div className="picContainer">
       <div className="service_pic">
-        <img src={media} alt={picName} />
+        <img src={servicePic} alt={picName} />
         <div className="service_pic_caption">
           <div className="service_pic_text">{picName}</div>
         </div>
