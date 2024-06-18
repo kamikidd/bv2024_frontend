58d43e61112cc50e95ec999d287e95d0c90e80a8
Youyou <puyouyou@gmail.com>
2024-06-18 18:28:46 +0200

    fixed header with offcanvas

--- src/pages/Others/Header.jsx
+++ src/pages/Others/Header.jsx
@@ -7,7 +7,7 @@ import TitlePictureComp from "./TitlePictureComp";
 import Container from "react-bootstrap/Container";
 import bv_logo from "../../assets/imgs/logos/bv_logo.svg";
 import side_logo from "../../assets/imgs/logos/side_logo.svg";
-
+import { Navbar, Offcanvas } from "react-bootstrap";
 const Header = () => {
   const location = useLocation();
   let page = location.pathname.substring(1);
@@ -17,159 +17,71 @@ const Header = () => {
 
   return (
     <Container fluid className="header_container">
-      <Container>
-        <Nav className="border-bottom navbar fixed-top bg-white navbar-expand-lg navbar-light flex-nowrap">
-          <Container>
-            <Container id="logo_nav_container">
-              <Link to="/" className="navbar-brand">
-                <img
-                  src={bv_logo}
-                  alt="logo"
-                  height="100px"
-                  className="main_logo"
-                />
-                <img
-                  src={side_logo}
-                  height="98px"
-                  alt="side_logo"
-                  className="side_logo align-bottom"
-                />
-              </Link>
-            </Container>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="fixed-top bg-white navbar-light flex-nowrap"
+      >
+        <Container className="align-items-end">
+          <Navbar.Brand id="logo_nav_container">
+            <Link to="/" className="navbar-brand">
+              <img
+                src={bv_logo}
+                alt="logo"
+                height="100px"
+                className="main_logo"
+              />
+              <img
+                src={side_logo}
+                height="98px"
+                alt="side_logo"
+                className="side_logo align-bottom"
+              />
+            </Link>
 
-            <div className="btn_container">
-              <Button
-                className="navbar-toggler custom-toggler "
-                type="button"
-                onClick={handleShow}
-              >
-                <span className="navbar-toggler-icon"></span>
-              </Button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul id="nav_btn" className="navbar-nav">
-                  <li className="nav-item">
-                    <NavLink
-                      reloadDocument
-                      activeclassname="active"
-                      className="nav-link"
-                      aria-current="page"
-                      to="/Dienstleistungen"
-                    >
-                      Dienstleistungen
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      reloadDocument
-                      to="/Mitarbeitende"
-                      activeclassname="active"
-                      className="nav-link "
-                    >
-                      Über uns
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      reloadDocument
-                      to="/Themen"
-                      activeclassname="active"
-                      className="nav-link"
-                    >
-                      Themen
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      reloadDocument
-                      to="/Projekte"
-                      activeclassname="active"
-                      className="nav-link"
-                    >
-                      Projekte
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      reloadDocument
-                      to="/Kontakt"
-                      activeclassname="active"
-                      className="nav-link"
-                    >
-                      Kontakt
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <Modal show={show} onHide={handleClose} className="modal show">
-              <Modal.Dialog className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <Modal.Header
-                    closeButton
-                    className="modal-header"
-                  ></Modal.Header>
-                  <Modal.Body>
-                    <ul className="navbar-nav">
-                      <li>
-                        <NavLink
-                          reloadDocument
-                          activeclassname="active"
-                          className="modalNav"
-                          aria-current="page"
-                          to="/Dienstleistungen"
-                        >
-                          Dienstleistungen
-                        </NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink
-                          reloadDocument
-                          to="/Mitarbeitende"
-                          activeclassname="active"
-                          className="modalNav"
-                        >
-                          Über uns
-                        </NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink
-                          reloadDocument
-                          to="/Themen"
-                          activeclassname="active"
-                          className="modalNav"
-                        >
-                          Themen
-                        </NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink
-                          reloadDocument
-                          to="/Projekte"
-                          activeclassname="active"
-                          className="modalNav"
-                        >
-                          Projekte
-                        </NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink
-                          reloadDocument
-                          to="/Kontakt"
-                          activeclassname="active"
-                          className="modalNav"
-                        >
-                          Kontakt
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </Modal.Body>
-                </div>
-              </Modal.Dialog>
-            </Modal>
-          </Container>
-        </Nav>
-      </Container>
+            <Navbar.Toggle
+              aria-controls={`offcanvasNavbar-expand-lg`}
+              className="mb-2"
+            />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
+              backDrop="true"
+              className="w-50"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title
+                  id={`offcanvasNavbarLabel-expand-lg`}
+                ></Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body className="justify-content-end mb-2">
+                <Nav className="navbar_column">
+                  <Nav.Link
+                    href="#/Dienstleistungen"
+                    aria-current="page"
+                    reloadDocument
+                  >
+                    Dienstleistungen
+                  </Nav.Link>
+                  <Nav.Link href="#/Mitarbeitende" reloadDocument>
+                    Über uns
+                  </Nav.Link>
+                  <Nav.Link href="#/Themen" reloadDocument>
+                    Themen
+                  </Nav.Link>
+                  <Nav.Link href="#/Projekte" reloadDocument>
+                    Projekte
+                  </Nav.Link>
+                  <Nav.Link href="#/Kontakt" reloadDocument>
+                    Kontakt
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
 
       <Container fluid className="title_pic_content">
         <TitlePictureComp page={page}></TitlePictureComp>
