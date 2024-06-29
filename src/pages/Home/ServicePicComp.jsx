2e58801ab6ff583412aa673c06b7ba2e75bee66e
Youyou <puyouyou@gmail.com>
2024-06-29 17:06:36 +0200

    try to fix bug

--- src/pages/Home/ServicePicComp.jsx
+++ src/pages/Home/ServicePicComp.jsx
@@ -1,12 +1,26 @@
-import { useFetchMedia } from "../../utils/useFetchMedia";
+import Spinner from "../Others/Spinner";
 import styles from "./servicepiccomp.module.css";
+import { useNavigate } from "react-router-dom";
 import forschung_pic from "../../assets/imgs/category/forschung.png";
+import { useQuery } from "@tanstack/react-query";
+import fetchData from "../../utils/fetchData";
 const ServicePicComp = ({ imgid, picName }) => {
-  let media = useFetchMedia(imgid, forschung_pic);
+  const navigate = useNavigate();
+  const media = useQuery(["media_pic", `media`, imgid], fetchData);
+  if (media.isLoading) {
+    return <Spinner></Spinner>;
+  }
+  if (media.isError) {
+    navigate("/NotMatch404");
+  }
   return (
     <div className={`${styles.pic_container}`}>
       <div className={`${styles.service_pic}`}>
-        <img src={media ?? forschung_pic} alt={picName} width="100%" />
+        <img
+          src={media.data.source_url ?? forschung_pic}
+          alt={picName}
+          width="100%"
+        />
         <div className={`${styles.service_pic_caption}`}>
           <div>{picName}</div>
         </div>
